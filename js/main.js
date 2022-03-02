9fbeece1f9aba4c5c642483967b9987907af4b29
Jordane Gengo <jordane@hive.fi>
2022-03-02 16:51:12 +0200

    fix. responsive issue with the form

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
+const email = document.querySelector('#email');
+const message = document.querySelector('#error');
+const errordiv = document.querySelector('#errordiv');
+
 const validateEmail = (email) => {
   const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   return email.match(re);
 }
 
 const error = (msg) => {
-  const email = document.querySelector('#email');
-  const message = document.querySelector('#error');
-
   message.innerHTML = msg;
-  message.style.display = "block";
+  errordiv.style.display = "block";
   message.style.color = "var(--clr-3)";
   email.style.border = "1px solid var(--clr-3)"
 }
 
 const success = () => {
-  const email = document.querySelector('#email');
-  const message = document.querySelector('#error');
-
   message.innerHTML = "Thank you!"
   message.style.color = "green"
   email.style.border = ""
+  errordiv.style.display = "block";
 }
 
 const send_form = () => {
